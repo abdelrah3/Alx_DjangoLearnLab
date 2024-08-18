a595642396c2931315a0e38f528fd335a25fc87a
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-08-18 17:19:30 +0300

    views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 relationship_app/list_books.html
+relationship_app/library_detail.html
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
