eaf94f4b0ee28686d2c01a0f4ecc4f17774801f7
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-08-18 17:22:33 +0300

    views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 relationship_app/list_books.html
 relationship_app/library_detail.html
 def list_books(request):
@@ -12,3 +17,28 @@ from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('library_list')  # Redirect to a page after login
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
