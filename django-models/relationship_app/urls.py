11440d0e809e2d865285b2256ed563d3709bf99c
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-08-18 22:34:44 +0300

    urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,8 @@ from .views import user_register
 from .views import admin_view
 from .views import librarian_view
 from .views import member_view
+from .views import add_book_view, edit_book_view, delete_book_view
+
 urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
@@ -16,5 +18,8 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', user_register, name='register'),
+    path('books/add/', add_book_view, name='add_book'),
+    path('books/edit/<int:book_id>/', edit_book_view, name='edit_book'),
+    path('books/delete/<int:book_id>/', delete_book_view, name='delete_book'),
     "views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
 ]
