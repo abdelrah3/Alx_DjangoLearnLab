46e58f8b1aa0855c5a2d44989d42c6f5d48feaee
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-08-25 21:50:58 +0300

    views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from .forms import ExampleForm
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, id=book_id)
