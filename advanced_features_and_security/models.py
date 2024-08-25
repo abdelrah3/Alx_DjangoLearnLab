483a963fd81a02fd10c65f1ad08de72d3a7ee548
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-08-25 21:20:50 +0300

    models.py

--- advanced_features_and_security/models.py
+++ advanced_features_and_security/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
