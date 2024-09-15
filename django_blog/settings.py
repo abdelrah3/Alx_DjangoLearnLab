89e5602b77c6efddf15cdbf9d486b43f5b1eaa25
abdelrah3 <ahmed.abdo.aa.1996@gmail.com>
2024-09-15 22:23:03 +0300

    settings.py

---
+++ django_blog/settings.py
@@ -0,0 +1,22 @@
+INSTALLED_APPS = [
+    ...
+    'blog',
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'your_database_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+STATIC_URL = '/static/'
+TEMPLATES = [
+    {
+        ...
+        'DIRS': [BASE_DIR / 'templates'],
+        ...
+    },
+]
